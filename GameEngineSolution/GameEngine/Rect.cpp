e38f55ff4d7770b709427e8fd4a94bc7d8636c38
Pedro Yan Ornelas <pedroyane@gmail.com>
2017-03-12 22:48:26 -0300

    Concluida implementação de rect e suas funções

--- GameEngineSolution/GameEngine/Rect.cpp
+++ GameEngineSolution/GameEngine/Rect.cpp
@@ -2,7 +2,50 @@
 
 
 
+Rect Rect::operator+(const Vec2 vec) {
+	Rect result;
+
+	result.X = X + vec.X;
+	result.Y = Y + vec.Y;
+	result.W = W;
+	result.H = H;
+
+	return result;
+}
+
+Vec2 Rect::GetCenter() {
+	Vec2 toReturn;
+
+	toReturn.X = X + W / 2;
+	toReturn.Y = Y + H / 2;
+
+	return toReturn;
+}
+
+float Rect::DistanceFrom(Rect otherRectangle) {
+	Vec2 centerFromThis = GetCenter();
+	Vec2 centerFromOther = otherRectangle.GetCenter();
+
+	return centerFromThis.GetDistance(centerFromOther);
+}
+
+bool Rect::IsInside(Vec2 coordinates) {
+	return coordinates.X >= X && coordinates.X <= X + W
+		&& coordinates.Y >= Y && coordinates.Y <= Y + H;
+}
+
 Rect::Rect() {
+	X = 0;
+	Y = 0;
+	W = 0;
+	H = 0;
+}
+
+Rect::Rect(int x, int y, int w, int h) {
+	X = x;
+	Y = y;
+	W = w;
+	H = h;
 }
 
 
--- GameEngineSolution/GameEngine/Rect.h
+++ GameEngineSolution/GameEngine/Rect.h
@@ -1,11 +1,44 @@
 #pragma once
+#include "Vec2.h"
 class Rect {
 	public:
+		//variaveis
+
+		int X;
+		int Y;
+		int W;
+		int H;
+
+		//Operadores
+		Rect operator+(const Vec2 vec);
+
+		//Método
+
+		/// <summary>
+		/// Calcula as coordenadas do centro do retângulo
+		/// </summary>
+		/// <returns>o centro do retângulo</returns>
+		Vec2 GetCenter();
+
+		/// <summary>
+		/// Calcula a distância entre o centro deste retângulo
+		/// e do retângulo de destino passado como parâmetro
+		/// </summary>
+		/// <param name="otherRectangle">retângulo de destino</param>
+		/// <returns>a distância entre os dois retângulos</returns>
+		float DistanceFrom(Rect otherRectangle);
+
+		/// <summary>
+		/// Verifica se o ponto passado se encontra
+		/// dentro do retângulo
+		/// </summary>
+		/// <param name="coordinates">ponto a ser verificado</param>
+		/// <returns>booleano indicando se o ponto se encontra dentro do retângulo</returns>
+		bool IsInside(Vec2 coordinates);
+
 		Rect();
+		Rect(int x, int y, int w, int h);
 		~Rect();
-		int x;
-		int y;
-		int w;
-		int h;
+		
 };
 
