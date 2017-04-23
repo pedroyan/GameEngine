7fa067f3ffbcfc50f310f3046e38bf4dca58f9cd
Pedro Yan Ornelas <pedroyane@gmail.com>
2017-04-23 11:51:14 -0300

    Criada classe text

---
+++ GameEngineSolution/GameEngine/Text.cpp
@@ -0,0 +1,8 @@
+#include "Text.h"
+
+
+Text::Text(string fontFile, int fontsize, TextStyle style, string text, SDL_Color color, int x, int y) {
+}
+
+Text::~Text() {
+}
---
+++ GameEngineSolution/GameEngine/Text.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+#include <SDL_ttf.h>
+#include "Rect.h"
+using std::string;
+class Text {
+	public:
+		enum TextStyle{SOLID,SHADED,BLENDED};
+		Text(string fontFile,int fontsize, TextStyle style,
+			 string text,SDL_Color color, int x = 0, int y =  0);
+		~Text();
+		
+		void Render(int cameraX = 0, int cameraY = 0);
+		void SetPos(int x, int y, bool centerX = false, bool centerY = false);
+		void SetText(string text);
+		void SetColor(SDL_Color color);
+		void SetStyle(TextStyle style);
+		void SetFontSize(int fontSize);
+
+	private:
+		void RemakeTexture();
+		TTF_Font* font;
+		SDL_Texture* texture;
+
+		string text;
+		TextStyle style;
+		int fontsize;
+		SDL_Color color;
+		Rect box;
+};
+
