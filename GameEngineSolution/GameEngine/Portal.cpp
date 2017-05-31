139a261291267ac973c724c885742c6c69702a25
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-05-31 01:11:02 -0300

    Implementada classe portal

--- GameEngineSolution/GameEngine/Portal.cpp
+++ GameEngineSolution/GameEngine/Portal.cpp
@@ -1,8 +1,31 @@
 #include "Portal.h"
 
 
+Portal::Portal(string text, Rect dimensions) : portalText("adam-cg-pro.regular.otf",12,Text::TextStyle::BLENDED, text, { 255,255,255,255 }){
+	box = dimensions;
+}
 
-Portal::Portal() {
+void Portal::Update(float dt) {
+}
+
+void Portal::Render() {
+	if (renderText) {
+		portalText.Render();
+	}
+}
+
+bool Portal::IsDead() {
+	return false;
+}
+
+void Portal::NotifyCollision(GameObject & other) {
+	if (other.Is("Player")) {
+		renderText = true;
+	}
+}
+
+bool Portal::Is(string type) {
+	return type == "Portal";
 }
 
 
--- GameEngineSolution/GameEngine/Portal.h
+++ GameEngineSolution/GameEngine/Portal.h
@@ -1,8 +1,18 @@
 #pragma once
 #include "GameObject.h"
+#include "Text.h"
 class Portal : public GameObject {
 	public:
-		Portal();
+		Portal(string text, Rect dimensions);
+		void Update(float dt);
+		void Render();
+		bool IsDead();
+		void NotifyCollision(GameObject& other);
+		bool Is(string type);
 		~Portal();
+
+	private:
+		Text portalText;
+		bool renderText;
 };
 
