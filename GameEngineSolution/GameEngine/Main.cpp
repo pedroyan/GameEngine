f30cae2051c622777201bcd6770ef426cdd97641
Carlos Tavares <carlost@lacunasoftware.com>
2017-06-05 18:58:46 -0300

    #23 A estrela

--- GameEngineSolution/GameEngine/Main.cpp
+++ GameEngineSolution/GameEngine/Main.cpp
@@ -4,17 +4,22 @@
 #include "StageState.h"
 #include "TitleState.h"
 #include "Resources.h"
+#include "Pathfinding.h"
 
 int wmain(int argc, char ** argv) {
-	int Height = DEFAULT_GAME_HEIGHT;
-	int Width = DEFAULT_GAME_WIDTH;
+	int a;
+	//int Height = DEFAULT_GAME_HEIGHT;
+	//int Width = DEFAULT_GAME_WIDTH;
 
 
-	Game* game = new Game("Jogo Final",Width,Height);
-	game->Push(new TitleState());
-	game->Run();
-	Game::ClearResources();
-	delete game;
+	//Game* game = new Game("Jogo Final",Width,Height);
+	//game->Push(new TitleState());
+	//game->Run();
+	//Game::ClearResources();
+	//delete game;
 
+	Pathfinding(Vec2(1, 4), Vec2(8, 5));
+
+	scanf("%d", &a);
 	return 0;
 }
---
+++ GameEngineSolution/GameEngine/Pathfinding.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <list>
+#include <unordered_map>
+#include <iostream>
+#include "PriorityQueue.h"
+#include "Vec2.h"
+
+using namespace std;
+
+list<Vec2> FindNeighbors(Vec2 node, int tileWidth, int tileHeight) {
+	list<Vec2> neighbors;
+	neighbors.push_back(Vec2(node.X + tileWidth, node.Y));
+	if (node.X - tileWidth >= 0) {
+		neighbors.push_back(Vec2(node.X - tileWidth, node.Y));
+	}
+
+	neighbors.push_back(Vec2(node.X, node.Y + tileHeight));
+	if (node.Y - tileHeight >= 0) {
+		neighbors.push_back(Vec2(node.X, node.Y - tileHeight));
+	}
+
+	return neighbors;
+}
+
+inline double heuristic(Vec2 a, Vec2 b) {
+	return abs(a.X - b.X) + abs(a.Y - b.Y);
+}
+
+struct Vec2Hasher {
+	std::size_t operator()(const Vec2& k) const {
+		using std::size_t;
+		using std::hash;
+		using std::string;
+
+		return (53 + hash<int>()(k.X)) * 53 + hash<int>()(k.Y);
+	}
+};
+
+std::ostream &operator<<(std::ostream &os, Vec2 const &m) {
+	return os << "X: " << m.X << " Y: " << m.Y;
+}
+
+void Run(Vec2 start, Vec2 destination, unordered_map<Vec2, Vec2, Vec2Hasher> came_from, unordered_map<Vec2, int, Vec2Hasher> cost_so_far){
+	auto current = destination;
+	while (current != start) {
+		std::cout << current << endl;
+		current = came_from[current];
+	}
+	std::cout << current << endl;
+}
+
+Vec2 Pathfinding(Vec2 start, Vec2 destination) {
+	PriorityQueue<Vec2, double> frontier;
+	frontier.put(start, 0);
+	unordered_map<Vec2, Vec2, Vec2Hasher> came_from;
+	unordered_map<Vec2, int, Vec2Hasher> cost_so_far;
+	came_from[start] = start;
+	cost_so_far[start] = 0;
+
+	while (!frontier.empty()) {
+		Vec2 current = frontier.get();
+
+		if (current == destination) {
+			break;
+		}
+
+		for (auto next : FindNeighbors(current, 1, 1)) {
+			double new_cost = cost_so_far[current] + current.GetDistance(next);
+			if (!cost_so_far.count(next) || new_cost < cost_so_far[next]) {
+				cost_so_far[next] = new_cost;
+				double priority = new_cost + heuristic(destination, next);
+				frontier.put(next, priority);
+				came_from[next] = current;
+			}
+		}
+	}
+
+	Run(start, destination, came_from, cost_so_far);
+
+	return came_from[destination];
+}
