e830b91ca092f1d1d0749b6ffdee17c787aca7ed
Carlos Tavares <carlost@lacunasoftware.com>
2017-06-20 19:45:46 -0300

    #70 Pathfinding inimigo

--- GameEngineSolution/GameEngine/Enemy.cpp
+++ GameEngineSolution/GameEngine/Enemy.cpp
@@ -1,5 +1,13 @@
 #include "Enemy.h"
 #include "Debug.h"
+#include "PriorityQueue.h"
+#include "TileCollision.h"
+#include "Pathfinding.h"
+#include "Game.h"
+
+const float jumpHeight = 2;
+const float Gravity = 2 * 9.8;
+const float sqrK1 = sqrt(2 * Gravity * jumpHeight);
 
 Enemy::~Enemy() {
 }
@@ -8,8 +16,87 @@ void Enemy::CreateDebugBox() {
 	Debug::MakeCenteredDebugSquare(box, { 250, 0, 0 });
 }
 
-void Enemy::MoveTo(Vec2 pos) {
-	//Pathfinding do carlos
+void Enemy::MoveTo(Vec2 pos, float dt) {
+	PriorityQueue<Vec2, double> frontier;
+	Vec2 newPos;
+	
+	auto tileHeight = Game::GetInstance().GetCurrentState().GetMap().GetTileSet()->GetTileHeight();
+	
+	auto neig = FindNeighbors(SpeedLimit * dt, tileHeight * sqrK1 * dt, pos);
+	
+	if (!neig.empty()) {
+		for (auto next : neig) {
+			frontier.put(next, heuristic(pos, next));
+		}
+		
+		while (!frontier.empty()) {
+			newPos = frontier.get();
+			Rect newPosBox = box;
+			newPosBox.X = newPos.X;
+			newPosBox.Y = newPos.Y;
+			auto collisionAnalysis = TileCollision::PriorityCollision(newPosBox, 0);
+			
+			if (collisionAnalysis == CollisionType::Solid) {
+				continue;
+			}
+			
+			if (collisionAnalysis == CollisionType::noCollision) {
+				if (box.Y > newPos.Y) { //quero pular
+					if (ground == 1 && speed.Y == 0) { //posso pular?
+						box.X = newPos.X;
+						speed.X = SpeedLimit;
+						speed.Y = -tileHeight * sqrK1;
+						break;
+						
+					} else {
+						continue;
+					}
+				}
+				else {
+					box.X = newPos.X;
+					speed.X = SpeedLimit;
+					break;
+				}
+			}
+		}
+	}
+
+	if (box.X != newPos.X) {//não andei
+		Rect newPosBox = box;
+		newPosBox.X = box.X + SpeedLimit * dt;
+		newPosBox.Y = box.Y + tileHeight * sqrK1;
+		auto collisionAnalysisR = TileCollision::PriorityCollision(newPosBox, 0);
+		
+		newPosBox.X = box.X - SpeedLimit * dt;
+		newPosBox.Y = box.Y + tileHeight * sqrK1;
+		auto collisionAnalysisL = TileCollision::PriorityCollision(newPosBox, 0);
+		if (collisionAnalysisL == CollisionType::noCollision || collisionAnalysisR == CollisionType::noCollision) {//se eu pular eu contorno
+			if (ground == 1 && speed.Y == 0) { //posso pular?
+				speed.Y = -tileHeight * sqrK1;
+			}
+		}
+	}
+	
+	if (box.X == pos.X && box.Y != pos.Y) {
+		if (speedStairs.Y == 0) {
+			if (box.Y > pos.Y) {
+				speedStairs.Y = -SpeedLimit / 2;
+			}
+			else {
+				speedStairs.Y = -SpeedLimit / 2;
+			}
+		}
+
+		Rect previousRect = box;
+		Rect stairsAnalisys = previousRect;
+		stairsAnalisys.Y += speedStairs.Y*dt;
+		stairsAnalisys.W = 0;
+		stairsAnalisys.X += box.W / 2;
+
+		auto collisionAnalysisLayer1 = TileCollision::PriorityCollision(stairsAnalisys, 1);
+		if (collisionAnalysisLayer1 == CollisionType::Stairs) {
+		}
+	}
 }
 
 void Enemy::Focus(Player* focus) {
@@ -23,4 +110,17 @@ Enemy::Enemy(Sprite stillSprite, Sprite walkingSprite) : stillSprite(stillSprite
 
 void Enemy::MoveToDumbly(Vec2 pos) {
 	//pathfinding tosco e menos custoso
+}
+
+std::list<Vec2> Enemy::FindNeighbors(float tileWidth, float tileHeight, Vec2 pos) {
+	list<Vec2> neighbors;
+	if (pos.X > box.X) {
+		neighbors.push_back(Vec2(box.X + tileWidth, box.Y));
+		neighbors.push_back(Vec2(box.X + tileWidth, box.Y - tileHeight));
+	} else if (pos.X < box.X) {
+		neighbors.push_back(Vec2(box.X - tileWidth, box.Y));
+		neighbors.push_back(Vec2(box.X - tileWidth, box.Y - tileHeight));
+	}
+	
+	return neighbors;
 }
--- GameEngineSolution/GameEngine/MeleeEnemy.cpp
+++ GameEngineSolution/GameEngine/MeleeEnemy.cpp
@@ -1,6 +1,8 @@
 #include "MeleeEnemy.h"
 #include "Camera.h"
 #include "Bullet.h"
+#include "TileCollision.h"
+#include "Game.h"
 
 float attackDuration = 1;
 
@@ -12,6 +14,7 @@ MeleeEnemy::MeleeEnemy(float x, float y) : Enemy(Sprite("img/MeleeEnemy.png"), S
 	box.W = actualSprite->GetWidth();
 	box.H = actualSprite->GetHeight();
 	attackRange = box.W;
+	ground = 1;
 }
 
 
@@ -20,14 +23,37 @@ MeleeEnemy::~MeleeEnemy() {
 
 void MeleeEnemy::Update(float dt) {
 	ApplyGravity(dt);
-	if (focus!= nullptr) {
-		MoveToDumbly(focus->box.GetCenter());
+
+	if (focus != nullptr) {
+		auto goindTo = Vec2(focus->box.X, focus->box.Y + (focus->box.H - box.H));
+
+		if (goindTo.X - box.X > SpeedLimit*dt) {
+			MoveTo(focus->box.GetCenter(), dt);
+		} else {
+			Rect newPosBox = box;
+			newPosBox.X = goindTo.X;
+			auto collisionAnalysisY = TileCollision::PriorityCollision(newPosBox, 0);
+			if (collisionAnalysisY == CollisionType::noCollision) {
+				box.X = goindTo.X;
+			}
+		}
+
 		CheckAttack(dt);
 	}
+
 	actualSprite->Update(dt);
-	auto result = MoveOnSpeed(dt);
-	if (result & (int)CollisionFlags::Bottom) {
-		Jump(3);
+	//auto result = MoveOnSpeed(dt);
+	auto tileHeight = Game::GetInstance().GetCurrentState().GetMap().GetTileSet()->GetTileHeight();
+	speed.Y += tileHeight * Gravity * dt;
+	box.Y += speed.Y*dt;
+	
+	auto collisionAnalysisY = TileCollision::PriorityCollision(this->box, 0);
+	if (collisionAnalysisY == CollisionType::Solid) {
+		box.Y -= speed.Y*dt;
+		ground = 1;
+		speed.Y = 0;
+	} else {
+		ground = 0;
 	}
 }
 
